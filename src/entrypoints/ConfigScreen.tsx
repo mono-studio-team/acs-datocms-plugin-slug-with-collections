d10eac65f36ea279af7a26864fd51e978d38d19b
Julclt <julien.collet01@gmail.com>
2023-04-19 02:01:17 +0400

    Setup the plugin for slug field

--- src/entrypoints/ConfigScreen.tsx
+++ src/entrypoints/ConfigScreen.tsx
@@ -1,18 +1,59 @@
-import { RenderConfigScreenCtx } from 'datocms-plugin-sdk';
-import { Canvas, ContextInspector } from 'datocms-react-ui';
-import s from './styles.module.css';
+import { RenderConfigScreenCtx } from "datocms-plugin-sdk";
+import { Button, Canvas, FieldGroup, Form, TextField } from "datocms-react-ui";
+import { Form as FormHandler, Field } from "react-final-form";
 
 type Props = {
   ctx: RenderConfigScreenCtx;
 };
 
+type ValidParameters = { readAPIToken: string };
+
 export default function ConfigScreen({ ctx }: Props) {
   return (
     <Canvas ctx={ctx}>
-      <p>Welcome to your plugin! This is your config screen!</p>
-      <div className={s.inspector}>
-        <ContextInspector />
-      </div>
+      <FormHandler<ValidParameters>
+        initialValues={ctx.plugin.attributes.parameters}
+        validate={(values) => {
+          const errors: Record<string, string> = {};
+          if (!values.readAPIToken) {
+            errors.readAPIToken = "This field is required!";
+          }
+          return errors;
+        }}
+        onSubmit={async (values) => {
+          await ctx.updatePluginParameters(values);
+          ctx.notice("Settings updated successfully!");
+        }}
+      >
+        {({ handleSubmit, submitting, dirty }) => (
+          <Form onSubmit={handleSubmit}>
+            <FieldGroup>
+              <Field name="readAPIToken">
+                {({ input, meta: { error } }) => (
+                  <TextField
+                    id="readAPIToken"
+                    label="Read-only API Token"
+                    hint="Settings > API Tokens > Read-only API Token"
+                    placeholder="Your read API token"
+                    required
+                    error={error}
+                    {...input}
+                  />
+                )}
+              </Field>
+            </FieldGroup>
+            <Button
+              type="submit"
+              fullWidth
+              buttonSize="l"
+              buttonType="primary"
+              disabled={submitting || !dirty}
+            >
+              Save settings
+            </Button>
+          </Form>
+        )}
+      </FormHandler>
     </Canvas>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
-import { connect } from 'datocms-plugin-sdk';
-import { render } from './utils/render';
-import ConfigScreen from './entrypoints/ConfigScreen';
-import 'datocms-react-ui/styles.css';
+import {
+  connect,
+  IntentCtx,
+  RenderFieldExtensionCtx,
+} from "datocms-plugin-sdk";
+import { render } from "./utils/render";
+import ConfigScreen from "./entrypoints/ConfigScreen";
+import "datocms-react-ui/styles.css";
+import "./index.css";
+import SlugWithCollections from "./components/SlugWithCollections";
 
 connect({
   renderConfigScreen(ctx) {
     return render(<ConfigScreen ctx={ctx} />);
+  },
+  manualFieldExtensions(ctx: IntentCtx) {
+    return [
+      {
+        id: "slugWithTreeCollections",
+        name: "Slug With Tree Collections",
+        type: "addon",
+        fieldTypes: ["slug"],
+      },
+    ];
+  },
+  renderFieldExtension(fieldExtensionId: string, ctx: RenderFieldExtensionCtx) {
+    switch (fieldExtensionId) {
+      case "slugWithTreeCollections":
+        return render(<SlugWithCollections ctx={ctx} />);
+    }
   },
 });
